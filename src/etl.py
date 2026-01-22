48b7b3451fff0d95e1e9561ebfc3e83ae6c808ef
JULIO JAVIER ROBLES GARCIA <jjroblesg@correo.url.edu.gt>
2026-01-22 13:11:48 -0600

    Solucion Lab 1 Julio Robles

--- src/etl.py
+++ src/etl.py
@@ -1,12 +1,26 @@
 import pandas as pd
 
 def run_etl():
-    """
-    Implementa el proceso ETL.
-    No cambies el nombre de esta función.
-    """
-    # TODO: implementar
-    pass
+    
+    #Leer datos
+    df = pd.read_csv('data/citas_clinica.csv')
+    
+    #Normalización de texto
+    df["paciente"] = df["paciente"].str.title()
+    df["especialidad"] = df["especialidad"].str.upper()
+    
+    #Validación de fechas
+    df["fecha_cita"] = df["fecha_cita"].str.replace("/", "-", regex=False)
+    df["fecha_cita"] = pd.to_datetime(df["fecha_cita"],errors="coerce",infer_datetime_format=True)
+
+
+    #Reglas del negocio
+    df = df[df["estado"] == "CONFIRMADA"]
+    df = df[df["costo"] > 0]
+    
+    #Teléfonos nulos
+    df["telefono"] = df["telefono"].fillna("NO REGISTRA")
+    df.to_csv('data/output.csv', index=False)
 
 
 if __name__ == "__main__":
